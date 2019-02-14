76e5f29e5ceff92f499546577977ba8ce4126408
tottu <tomoya.tozuka@gmail.com>
2019-02-14 22:09:14 +0900

    css add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [ 
-    AppComponent 
+  declarations: [
+    AppComponent
   ],
-  bootstrap:    [ 
-    AppComponent 
+  bootstrap: [
+    AppComponent
   ]
 })
 export class AppModule { }
