03876e725f307b2e1b2514e29acc982c77ff6891
tottu <tomoya.tozuka@gmail.com>
2019-02-15 23:40:57 +0900

    Move Member to exoteric file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Member } from './member';
-
-
-
-const MEMBERS: Member[] = [
-  { id: 11, name: '武山 岳大' },
-  { id: 12, name: '駒倉 光紀' },
-  { id: 13, name: '長田 研太' },
-  { id: 14, name: '高藤 友梨香' },
-  { id: 15, name: '浜崎 貴之' },
-  { id: 16, name: '緑川 睦' },
-  { id: 17, name: '森谷 怜奈' },
-  { id: 18, name: '大槻 祐大' },
-  { id: 19, name: '岩野 紀之' },
-  { id: 20, name: '佐々木 小次郎' },
-];
+import { MemberService } from './member.service';
 
 
 @Component({
@@ -79,15 +65,28 @@ const MEMBERS: Member[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-  
-  `]
+  `],
+  providers : [ MemberService ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = '自社社員名簿';
-  members = MEMBERS;
+  members : Member[];
   selectedMember: Member;
 
+  constructor(private memberService: MemberService) { 
+    
+  }
+
+  ngOnInit() : void{
+    //constructorが呼ばれた後に実行されるコールバック
+    this.gerMembers();
+  }
+  
   onSelect(member: Member): void {
     this.selectedMember = member;
+  }
+
+  gerMembers() : void {
+    this.memberService.getMembers().then(members => this.members = members);
   }
 }
