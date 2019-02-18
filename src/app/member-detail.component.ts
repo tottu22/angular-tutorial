fc30da1dd5f4afeb761ca3ec5e42da2b07dbe76a
tottu <tomoya.tozuka@gmail.com>
2019-02-18 22:55:56 +0900

    Add registration function

--- src/app/member-detail.component.ts
+++ src/app/member-detail.component.ts
@@ -32,5 +32,10 @@ export class MemberDetailComponent implements OnInit{
     goBack(): void {
         this.location.back();
     }
+
+    save(): void {
+        this.memberService.update(this.member)
+        .then(() => this.goBack());
+    }
 }
 
